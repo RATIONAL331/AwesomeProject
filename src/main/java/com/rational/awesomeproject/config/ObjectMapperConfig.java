89239ff0830d99d419a6f1f1de59f7f6c8b85dd9
RATIONAL331 <rational331@gmail.com>
2023-03-13 22:50:36 +0900

    Default config

---
+++ src/main/java/com/rational/awesomeproject/config/ObjectMapperConfig.java
@@ -0,0 +1,20 @@
+package com.rational.awesomeproject.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ObjectMapperConfig {
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, Boolean.FALSE);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, Boolean.FALSE);
+		return objectMapper;
+	}
+}
---
+++ src/main/java/com/rational/awesomeproject/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.rational.awesomeproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http.authorizeExchange()
+		    .anyExchange().permitAll()
+		    .and()
+		    .httpBasic()
+		    .and()
+		    .formLogin();
+		return http.build();
+	}
+
+	@Bean
+	public MapReactiveUserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user")
+		                       .password("password")
+		                       .roles("USER")
+		                       .build();
+		return new MapReactiveUserDetailsService(user);
+	}
+}
