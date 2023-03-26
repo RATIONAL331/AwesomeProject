f82ec30e8b794e31e3305b89e35ff6b48bd6c3f6
RATIONAL331 <rational331@gmail.com>
2023-03-26 21:57:42 +0900

    스토리지 테스트 코드 일부 추가

--- src/test/java/com/rational/awesomeproject/service/impl/AwesomeStorageServiceImplTest.java
+++ src/test/java/com/rational/awesomeproject/service/impl/AwesomeStorageServiceImplTest.java
@@ -2,13 +2,21 @@ package com.rational.awesomeproject.service.impl;
 
 import com.rational.awesomeproject.repository.AwesomeStorageReactiveRepository;
 import com.rational.awesomeproject.repository.AwesomeUserReactiveRepository;
+import com.rational.awesomeproject.repository.model.AwesomeStorage;
+import com.rational.awesomeproject.repository.model.AwesomeUser;
 import com.rational.awesomeproject.service.FileStorageService;
+import com.rational.awesomeproject.service.dto.AwesomeStorageDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
 class AwesomeStorageServiceImplTest {
@@ -21,9 +29,71 @@ class AwesomeStorageServiceImplTest {
 	@Mock
 	private FileStorageService fileStorageService;
 
+	@BeforeEach
+	public void setUp() {
+		AwesomeUser data = AwesomeUser.makeDefault("name", "pwd");
+		data.setId("1");
+		data.setRootStorageId("1");
+		Mockito.lenient()
+		       .when(userReactiveRepository.findById(Mockito.anyString()))
+		       .thenReturn(Mono.just(data));
+	}
+
 	@Test
-	@DisplayName("폴더 생성하기 테스트")
+	@DisplayName("폴더 생성하기 테스트 - 일반적인 경우")
 	void makeFolder() {
+		AwesomeStorage rootStorage = AwesomeStorage.makeDefault("1");
+		rootStorage.setId("1");
+
+		Mockito.when(storageReactiveRepository.findByIdAndUserId(Mockito.anyString(), Mockito.anyString()))
+		       .thenReturn(Mono.just(rootStorage));
+		Mockito.when(storageReactiveRepository.findAllByParentStorageIdAndDeletedAtIsNull(Mockito.anyString()))
+		       .thenReturn(Flux.just(
+				       AwesomeStorage.makeFolder("1", "1", "test"),
+				       AwesomeStorage.makeFolder("1", "1", "test2"),
+				       AwesomeStorage.makeFile("1", "1", "test3", 1L)));
+
+		Mockito.when(storageReactiveRepository.save(Mockito.any(AwesomeStorage.class)))
+		       .thenReturn(Mono.just(AwesomeStorage.makeFolder("1", "1", "test4")));
+
+		Mono<AwesomeStorageDto> test = awesomeStorageService.makeFolder("1", "1", "test4");
+		test.as(StepVerifier::create)
+		    .expectNextMatches(awesomeStorageDto -> awesomeStorageDto.getStorageName().equals("test4"))
+		    .verifyComplete();
+	}
+
+	@Test
+	@DisplayName("폴더 생성하기 테스트 - 중복된 이름의 폴더를 생성하는 경우")
+	void makeFolderDup() {
+		AwesomeStorage rootStorage = AwesomeStorage.makeDefault("1");
+		rootStorage.setId("1");
+
+		Mockito.when(storageReactiveRepository.findByIdAndUserId(Mockito.anyString(), Mockito.anyString()))
+		       .thenReturn(Mono.just(rootStorage));
+		Mockito.when(storageReactiveRepository.findAllByParentStorageIdAndDeletedAtIsNull(Mockito.anyString()))
+		       .thenReturn(Flux.just(
+				       AwesomeStorage.makeFolder("1", "1", "test"),
+				       AwesomeStorage.makeFolder("1", "1", "test2"),
+				       AwesomeStorage.makeFile("1", "1", "test3", 1L)));
+
+		Mono<AwesomeStorageDto> test = awesomeStorageService.makeFolder("1", "1", "test");
+		test.as(StepVerifier::create)
+		    .verifyComplete();
+
+		Mockito.verify(storageReactiveRepository, Mockito.never()).save(Mockito.any(AwesomeStorage.class));
+	}
+
+
+	@Test
+	@DisplayName("폴더 생성하기 테스트 - 사용자에게 없는 폴더를 상대로 생성하는 경우")
+	void makeFolderNotAuthor() {
+		Mockito.when(storageReactiveRepository.findByIdAndUserId(Mockito.anyString(), Mockito.anyString()))
+		       .thenReturn(Mono.empty());
+
+		Mono<AwesomeStorageDto> test = awesomeStorageService.makeFolder("1", "1", "test4");
+		test.as(StepVerifier::create)
+		    .expectErrorMessage("Not Exist File/Folder")
+		    .verify();
 	}
 
 	@Test
@@ -49,15 +119,54 @@ class AwesomeStorageServiceImplTest {
 	@Test
 	@DisplayName("최상위 폴더 가져오기 테스트")
 	void getRootFolder() {
+		AwesomeUser data = AwesomeUser.makeDefault("name", "pwd");
+		data.setRootStorageId("12345");
+		Mockito.when(userReactiveRepository.findById(Mockito.anyString()))
+		       .thenReturn(Mono.just(data));
+
+		AwesomeStorage rootStorage = AwesomeStorage.makeDefault("1");
+		rootStorage.setId("12345");
+		Mockito.when(storageReactiveRepository.findById("12345"))
+		       .thenReturn(Mono.just(rootStorage));
+
+		awesomeStorageService.getRootStorageByUserId("1")
+		                     .as(StepVerifier::create)
+		                     .expectNextMatches(awesomeStorageDto -> awesomeStorageDto.getId().equals("12345"))
+		                     .verifyComplete();
 	}
 
 	@Test
 	@DisplayName("폴더 내용 가져오기 테스트")
 	void getFolderContent() {
+		AwesomeStorage rootStorage = AwesomeStorage.makeDefault("1");
+		rootStorage.setId("1");
+
+		Mockito.when(storageReactiveRepository.findByIdAndUserId(Mockito.anyString(), Mockito.anyString()))
+		       .thenReturn(Mono.just(rootStorage));
+		Mockito.when(storageReactiveRepository.findAllByParentStorageIdAndDeletedAtIsNull(Mockito.anyString()))
+		       .thenReturn(Flux.just(
+				       AwesomeStorage.makeFolder("1", "1", "test"),
+				       AwesomeStorage.makeFolder("1", "1", "test2"),
+				       AwesomeStorage.makeFile("1", "1", "test3", 1L)));
+
+		awesomeStorageService.getStorageByParentStorageId("1", "1")
+		                     .as(StepVerifier::create)
+		                     .expectNextCount(3)
+		                     .verifyComplete();
 	}
 
 	@Test
 	@DisplayName("폴더 정보 가져오기 테스트")
 	void getFolderInfo() {
+		AwesomeStorage rootStorage = AwesomeStorage.makeDefault("1");
+		rootStorage.setId("1");
+
+		Mockito.when(storageReactiveRepository.findByIdAndUserId(Mockito.anyString(), Mockito.anyString()))
+		       .thenReturn(Mono.just(rootStorage));
+
+		awesomeStorageService.getStorageById("1", "1")
+		                     .as(StepVerifier::create)
+		                     .expectNextMatches(awesomeStorageDto -> awesomeStorageDto.getId().equals("1"))
+		                     .verifyComplete();
 	}
 }
