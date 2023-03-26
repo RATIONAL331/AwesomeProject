3b93d80fef85944321a0a6aade7f5485d485ae18
RATIONAL331 <rational331@gmail.com>
2023-03-26 13:57:22 +0900

    불필요한 테스트 제거

--- src/test/java/com/rational/awesomeproject/AwesomeProjectApplicationTests.java
+++ src/test/java/com/rational/awesomeproject/AwesomeProjectApplicationTests.java
@@ -1,14 +1,9 @@
 package com.rational.awesomeproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AwesomeProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-
-	}
 
 }
