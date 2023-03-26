bdd4f94876ce6c14808e6fecec62e67e48d37031
RATIONAL331 <rational331@gmail.com>
2023-03-26 17:24:03 +0900

    mongo db uri modify

--- src/test/java/com/rational/awesomeproject/AwesomeProjectApplicationTests.java
+++ src/test/java/com/rational/awesomeproject/AwesomeProjectApplicationTests.java
@@ -1,9 +1,13 @@
 package com.rational.awesomeproject;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AwesomeProjectApplicationTests {
 
+	@Autowired
+	private StringEncryptor encryptor;
 
 }
